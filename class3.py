3f767683d40922a599f207c486d5eaa033ace22e
Hearty135 <heartymangeni@gmail.com>
2025-10-13 16:48:48 +0300

    Add files via upload

---
+++ class3.py
@@ -0,0 +1,41 @@
+# class Student:
+#    def __init__(self, name):
+#     self.name = name      #public
+#     self._gpa = 3.5       #protected
+#     self.__password = "1234"   #private
+
+# student = Student("Zakaria John")
+# print(student.name)
+# print(student._gpa)
+# #print(student.__password)
+
+# student.name ="Mercy"
+# print(student.name)
+
+coins = 100
+coins2= 500+100
+print (coins2)
+
+class Piggybank:
+    def __init__(self,coins):
+        self._coins=coins
+        self.put_in(coins)
+
+    def put_in(self, amount):
+        if amount <= 0:
+            raise ValueError("Add real money")
+        self._coins += amount
+
+    def take_out(self, amount):
+        if amount <=0:
+            raise ValueError("Be for real")
+        if amount > self.coins:
+            raise ValueError("Money has come")
+        self._coins -= amount
+    
+    def how_much(self):
+        return self._coins
+
+Margie= Piggybank(1000000)
+Margie.put_in(3000)
+print("Margie's box has:",Margie.how_much(),"coins")
